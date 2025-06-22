f8aad12400fc01ec8115e2e1bb0a0d193c63b4ee
Hemanth <rachapalli.hemanth5544@gmail.com>
2025-06-22 09:09:12 +0530

    rds & routes

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,14 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Contact } from '../models/Contact';
-
+import dotenv from 'dotenv';
+dotenv.config(); 
 const sequelize = new Sequelize({
-  database: process.env.DB_NAME || 'bitespeed',
+  database: process.env.DB_NAME,
   dialect: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST ,
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   models: [Contact],
 });
 
